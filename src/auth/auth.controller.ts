b62b9e5a7115dc1d55b9d138d9825108f3a92acd
mnsdojo <manishkumar97430@gmail.com>
2024-12-27 23:24:33 +0530

    updated auth controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,41 +1,27 @@
+import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import {
-  Body,
-  Controller,
-  Get,
-  HttpException,
-  HttpStatus,
-  Post,
-  Req,
-  Request,
-  UseGuards,
-} from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
-import { AuthPayloadDto } from 'src/dto/auth.dto';
-import { JwtAuthGuard } from 'src/guards/jwt.guard';
-import { LocalGuard } from 'src/guards/local.guard';
+import { LoginDto, RegisterDto } from './dto/auth.dto';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { GetUser } from './decorators/get-user.decoratorr';
+import { User } from './entities/user.entity';
 
-@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private authService: AuthService) {}
 
-  @Post('login')
-  @UseGuards(LocalGuard)
-  @ApiOperation({ summary: 'Login User' })
-  @ApiBody({ type: AuthPayloadDto })
-  @ApiResponse({
-    status: 200,
-    description: 'Login Success',
-  })
-  login(@Request() req: Request, @Body() authPayload: AuthPayloadDto) {
-    const user = this.authService.validateUser(authPayload);
-    return user;
+  @Post('register')
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
   }
 
-  @Get('status')
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @Get('profile')
   @UseGuards(JwtAuthGuard)
-  status(@Req() req: Request) {
-    console.log(req);
+  getProfile(@GetUser() user: User) {
+    return user;
   }
 }
